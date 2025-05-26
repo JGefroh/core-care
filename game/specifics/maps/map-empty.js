2b3002eeae57483b5d5aed3ffc8f4f132cf9748f
Joseph Gefroh <joseph.gefroh@gmail.com>
2025-05-26 15:54:58 -0700

    [bg] mountains

--- game/specifics/maps/map-empty.js
+++ game/specifics/maps/map-empty.js
@@ -1,7 +1,7 @@
 const map = {
     floors: [
-        {key: 'SKY', xPosition: 0, yPosition: -500, width: 1000, height: 160, color: '#4FC3F7', renderLayer: 'SKY' }, // Sky
-        {xPosition: 0, yPosition: -370, width: 1000, height: 100, color: '#FF00FF' }, // Mountain
+        {key: 'SKY', xPosition: 0, yPosition: -500, width: 1000, height: 320, color: '#4FC3F7', renderLayer: 'SKY' }, // Sky
+        {xPosition: 0, yPosition: -370, width: 1000, height: 100, color: '#FF00FF', imageKey: 'BG_MOUNTAIN' }, // Mountain
     ],
     walls: [
 
