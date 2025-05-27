d9602d9ae996647d092437fe7170c122e7ce18b3
Joseph Gefroh <joseph.gefroh@gmail.com>
2025-05-26 17:05:34 -0700

    [map] enclose boundarie even more

--- game/specifics/maps/map-empty.js
+++ game/specifics/maps/map-empty.js
@@ -5,8 +5,8 @@ const map = {
         {xPosition: 0, yPosition: -270, width: 1100, height: 260, color: '#FF00FF', imageKey: 'BG_TREELINE' }, // Mountain
     ],
     walls: [
-        {from: [-600,-340], to: [500,-340], collision: 'WALL', color: 'rgba(0,0,0,0)'},
-        {offset: [0, 500], color: 'rgba(0,0,0,0)'},
+        {from: [-600,-340], to: [400,-340], collision: 'WALL', color: 'rgba(0,0,0,0)'},
+        {offset: [0, 250], color: 'rgba(0,0,0,0)'},
         {offset: [-910, 0], color: 'rgba(0,0,0,0)'},
         {offset: [0, -500], color: 'rgba(0,0,0,0)'},
     ],
